f4bd7ad9f9b36120cbd25ea633e3839d8398a204
carlos stefano <carlos.stefano@hotmail.com.br>
2018-01-07 23:45:06 -0200

    consegui fazer multiplos requests usando await

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('./controller.js')
-var controller = new controller()
 
 router.get('/',(req,res)=>{
     controller.connect(res);
